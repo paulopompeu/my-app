b8bac4ec2b31e45876b92bf00be1fb74794cba96
Paulo Pompeu Toledo de Oliveira <paulopompeu@Paulos-MBP.attlocal.net>
2020-03-27 14:05:38 -0500

    class 86

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,13 @@
-import React from "react";
+import React from 'react';
 
-import classes from "./Person.css";
+import classes from './Person.css';
 
-
-const person = props => { 
-
+const person = props => {
+  console.log('[Person.js] rendering...');
   return (
     <div className={classes.Person}>
       <p onClick={props.click}>
-        I'm {props.name} and I'm {props.years} years old!
+        I'm {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
