15e9f15efc3c2e965e072c7af96ee1f01cf8a4fb
Paulo Pompeu Toledo de Oliveira <paulopompeu@Paulos-MBP.attlocal.net>
2020-03-27 16:00:31 -0500

    rendering adjustments

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,49 +1,50 @@
-import React, {useEffect } from 'react';
+import React, { useEffect } from "react";
 
-import classes from './Cockpit.css';
+import classes from "./Cockpit.css";
 
-const cockpit = ( props ) => {
-    useEffect(() => {
-        console.log('[Cockpit.js] useEffect')
-        //Http request..
-        setTimeout(() => {
-            alert('save something to cloud')
-        }, 1000)
-        return () => {
-            console.log('[Cockpit.js] cleanup work in useEffect')
-        }
-    }, [])  // execs only the first time
-    // }, [props.persons])  // 
-    
-    useEffect(() => {
-        console.log('[Cockpit.js] 2nd useEffect')
-        return () => {
-            console.log('[Cockpit.js] cleanup work in 2nd useEffect')
-        }
-    })
+const cockpit = props => {
+  useEffect(() => {
+    console.log("[Cockpit.js] useEffect");
+    //Http request..
+    const timer = setTimeout(() => {
+      alert("save something to cloud");
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+      console.log("[Cockpit.js] cleanup work in useEffect");
+    };
+  }, []); // execs only the first time
+  // }, [props.persons])  //
 
-    const assignedClasses = [];
-    let btnClass = '';
-    if (props.showPersons) {
-        btnClass = classes.Red;
-    }
+  useEffect(() => {
+    console.log("[Cockpit.js] 2nd useEffect");
+    return () => {
+      console.log("[Cockpit.js] cleanup work in 2nd useEffect");
+    };
+  });
 
-    if ( props.persons.length <= 2 ) {
-      assignedClasses.push( classes.red ); // classes = ['red']
-    }
-    if ( props.persons.length <= 1 ) {
-      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
-    }
+  const assignedClasses = [];
+  let btnClass = "";
+  if (props.showPersons) {
+    btnClass = classes.Red;
+  }
 
-    return (
-        <div className={classes.Cockpit}>
-            <h1>{props.title}</h1>
-            <p className={assignedClasses.join( ' ' )}>This is really working!</p>
-            <button
-                className={btnClass}
-                onClick={props.clicked}>Toggle Persons</button>
-        </div>
-    );
+  if (props.personsLength <= 2) {
+    assignedClasses.push(classes.red); // classes = ['red']
+  }
+  if (props.personsLength <= 1) {
+    assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+  }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(" ")}>This is really working!</p>
+      <button className={btnClass} onClick={props.clicked}>
+        Toggle Persons
+      </button>
+    </div>
+  );
 };
 
-export default cockpit;
+export default React.memo(cockpit);
